f176669e9c930e385bc223a2527020470c223bb5
Pavel K <pavel@gamingtec.com>
2017-03-08 07:05:07 +0200

    Updated dependencies and version
    had to include dist files to be able to install from git
    set version to 0.1.4 because of npm install errors if 0.1.3.1 version used

---
+++ lib/bindings.d.ts
@@ -0,0 +1,20 @@
+import * as Vue from 'vue';
+import { mapState, mapGetters, mapActions, mapMutations } from 'vuex';
+export declare type VuexDecorator = <V extends Vue>(proto: V, key: string) => void;
+export declare type StateTransformer = (state: any, getters: any) => any;
+export declare type MapHelper = typeof mapState | typeof mapGetters | typeof mapActions | typeof mapMutations;
+export interface BindingOptions {
+    namespace?: string;
+}
+export interface BindingHelper {
+    <V extends Vue>(proto: V, key: string): void;
+    (type: string, options?: BindingOptions): VuexDecorator;
+}
+export interface StateBindingHelper extends BindingHelper {
+    (type: StateTransformer, options?: BindingOptions): VuexDecorator;
+}
+export declare const State: StateBindingHelper;
+export declare const Getter: BindingHelper;
+export declare const Action: BindingHelper;
+export declare const Mutation: BindingHelper;
+export declare function namespace<T extends BindingHelper>(namespace: string, helper: T): T;
